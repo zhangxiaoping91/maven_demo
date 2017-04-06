0ae839dfa4045a8f3f18404b93292c498803a959
zhangxiaoping <zhangxiaoping91@163.com>
2017-04-06 14:22:39 +0800

    demo配置分离

---
+++ demo-web/src/main/java/cn/creditease/controller/DemoController.java
@@ -0,0 +1,11 @@
+package cn.creditease.controller;
+
+/**
+ * @author ：xiaopingzhang8@creditease.cn
+ * @Description ：
+ * @ClassName ：DemoController
+ * @date ：2017/3/1 18:29
+ */
+//@RestController
+public class DemoController {
+}
